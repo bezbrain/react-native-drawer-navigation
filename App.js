741feebdfc65018154c7ff9c20034712f0f30d87
bezbrain <belloquadriolawale@gmail.com>
2024-04-21 10:22:57 +0100

    Style the drawer navigation and add drawer icons

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaView, Text } from "react-native";
+import "react-native-gesture-handler";
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { AntDesign } from "@expo/vector-icons";
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
   return (
     <NavigationContainer>
-      <Text>Todo...</Text>
+      <Drawer.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerStyle: { backgroundColor: "red" },
+          headerTintColor: "white",
+          drawerStyle: { backgroundColor: "#ccc" },
+          drawerActiveBackgroundColor: "red",
+          drawerActiveTintColor: "white",
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Home",
+            drawerIcon: ({ color, size }) => {
+              return <AntDesign name="home" size={size} color={color} />;
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerLabel: "New user",
+            drawerIcon: ({ color, size }) => {
+              return <AntDesign name="adduser" size={size} color={color} />;
+            },
+          }}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
