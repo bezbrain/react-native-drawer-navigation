7236999c9316920df46ccde7be15262a91f1229d
bezbrain <belloquadriolawale@gmail.com>
2024-04-21 10:34:44 +0100

    Toggle the drawer

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,11 +1,20 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 function UserScreen() {
+  const navigation = useNavigation();
+
+  const handleOpenDrawer = () => {
+    navigation.navigate("Welcome");
+    navigation.toggleDrawer();
+  };
+
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title="Go to welcome" onPress={handleOpenDrawer} />
     </View>
   );
 }
@@ -15,11 +24,11 @@ export default UserScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   highlight: {
-    fontWeight: 'bold',
-    color: '#eb1064',
+    fontWeight: "bold",
+    color: "#eb1064",
   },
 });
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 function WelcomeScreen() {
+  const navigation = useNavigation();
+
+  const handleGoToUser = () => {
+    navigation.navigate("User");
+    navigation.toggleDrawer();
+  };
+
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"Welcome"</Text> screen!
       </Text>
+      <Button title="Go to user" onPress={handleGoToUser} />
     </View>
   );
 }
@@ -15,11 +24,11 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   highlight: {
-    fontWeight: 'bold',
-    color: '#eb1064',
+    fontWeight: "bold",
+    color: "#eb1064",
   },
 });
