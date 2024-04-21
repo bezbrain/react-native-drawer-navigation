0d22b7b320a88d66d8c9628f9aeaa82dde5e20f1
bezbrain <belloquadriolawale@gmail.com>
2024-04-21 09:28:04 +0100

    Install the drawer packages

---
+++ App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, Text } from "react-native";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text>Todo...</Text>
+    </NavigationContainer>
+  );
+}
